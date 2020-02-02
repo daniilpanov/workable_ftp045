3dde65e285e201579b65adbb966899754849e4cf
daniil <daniil_panov@mail.ru>
2020-02-02 22:27:10 +0300

    I have a prototype of carousel!

--- tmp/carousel.php
+++ tmp/carousel.php
@@ -7,55 +7,91 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 
-    <link rel="stylesheet" href="../css/bootstrap/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/bootstrap/bootstrap-reboot.min.css">
-    <link rel="stylesheet" href="../css/bootstrap/bootstrap-grid.min.css">
-
     <script src="../js/jquery.min.js"></script>
-    <script src="../js/bootstrap/bootstrap.min.js"></script>
-    <script src="../js/bootstrap/bootstrap.bundle.min.js"></script>
+
+    <style>
+        #carousel
+        {
+
+        }
+
+        #carousel-inner
+        {
+
+        }
+
+        #control
+        {
+
+        }
+
+        #carousel-indicators
+        {
+
+        }
+
+        #carousel-indicators .indicator
+        {
+
+        }
+
+        #prev
+        {
+
+        }
+
+        #next
+        {
+
+        }
+
+        .slide
+        {
+
+        }
+
+        .slide.active
+        {
+
+        }
+    </style>
+
+    <script>
+        $(document).ready(function ()
+        {
+            var carousel = $("#carousel");
+            var indicators = $("#carousel-indicators");
+            var inner = $("#carousel-inner");
+            var slides = $(".slide");
+            var active_slide = $(".slide.active");
+            var prev = $("#prev");
+            var next = $("#next");
+
+
+        });
+    </script>
+
 </head>
 <body>
-<!-- Контент страницы -->
-<div class="container text-center">
-    <h1 class="h3" style="margin-top: 20px; margin-bottom: 5px;">Bootstrap 3 - Carousel (карусель)</h1>
-    <h2 class="h4" style="margin-top: 0; margin-bottom: 20px;">без элементов управления</h2>
-
-    <!-- Bootstrap 4 -->
-    <div id="carousel" class="carousel slide carousel-fade" data-ride="carousel">
-        <!-- Индикаторы -->
-        <ol class="carousel-indicators">
-            <li data-target="#carousel" data-slide-to="0" class="active"></li>
-            <li data-target="#carousel" data-slide-to="1"></li>
-            <li data-target="#carousel" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img class="img-fluid" src="../files/upload/images/for_2.1.png" alt="...">
-                <div class="carousel-caption">
-                    11111111
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img class="img-fluid" src="../files/upload/images/for_2.2.png" alt="...">
-
-            </div>
-            <div class="carousel-item">
-                <img class="img-fluid" src="../files/upload/images/for_2.3.png" alt="...">
-                <div class="carousel-caption">
-                    33333333
-                </div>
-            </div>
+<div id="carousel">
+    <div id="carousel-inner">
+        <div class="slide active">
+            <img src="../files/upload/images/for_2.1.png" alt="...">
         </div>
-        <!-- Элементы управления -->
-        <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Предыдущий</span>
-        </a>
-        <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Следующий</span>
-        </a>
+        <div class="slide">
+            <img src="../files/upload/images/for_2.1.png" alt="...">
+        </div>
+        <div class="slide">
+            <img src="../files/upload/images/for_2.1.png" alt="...">
+        </div>
+    </div>
+
+    <div id="control">
+        <div id="carousel-indicators">
+        </div>
+
+        <span id="prev"><</span>
+        <span id="next">></span>
     </div>
 </div>
 </body>
--- tmp/normal-carousel.php
+++ tmp/normal-carousel.php
@@ -16,26 +16,35 @@
     <script src="../js/bootstrap/bootstrap.bundle.min.js"></script>
 
     <style>
+        .carousel-outer
+        {
+            box-shadow: 0 0 20px 10px black;
+            width: 100%;
+            height: 100%;
+        }
+
         .carousel
         {
-            display: none;
             position: fixed;
-
-            width: 75%;
-            height: 75%;
-
+            margin: auto;
             top: 0;
             bottom: 0;
             left: 0;
             right: 0;
+
+            display: none;
+
+            background-color: rgba(0, 0, 0, 0.25);
+            box-shadow: inset 0 0 20em 10em black;
+
+            padding: 10em 25em;
         }
 
         .carousel .carousel-inner
         {
             height: 100%;
+            vertical-align: middle;
         }
-
-
 
         .carousel.visible
         {
@@ -51,15 +60,27 @@
                 var clickImg = (function ()
                 {
                     var el = $(this);
-                    $("body").css("overflow-y", "hidden");
                     el.unbind("click");
 
                     if (el.attr("aria-label"))
                     {
-                        console.log("opening");
                         var label = el.attr("aria-label");
                         var div = $(".carousel[aria-label=" + label + "]");
                         var closable = true;
+
+                        var imgs = $(".carousel[aria-label=" + label + "] img");
+
+                        var widths = [];
+                        var heights = [];
+
+                        imgs.each(function (index, element)
+                        {
+                            widths[index] = element.width;
+                            heights[index] = element.height;
+                        });
+
+                        div.width(Math.max.apply(null, widths));
+                        div.height(Math.max.apply(null, heights));
 
                         div.addClass("visible");
 
@@ -73,10 +94,8 @@
                             {
                                 if (closable)
                                 {
-                                    console.log("closing");
                                     div.removeClass("visible");
                                     el.click(clickImg);
-                                    $("body").css("overflow-y", "auto");
                                     $("body").unbind("click");
                                 }
                                 else
@@ -93,47 +112,47 @@
 <body>
 <!-- Контент страницы -->
 <div class="container text-center">
-    <img class="img-fluid" src="../files/upload/images/for_2.1.png" alt="...">
-    <img class="img-fluid" src="../files/upload/images/for_2.1.png" alt="...">
-    <img class="img-fluid" src="../files/upload/images/for_2.1.png" alt="..." aria-label="4">
+    <img src="../files/upload/images/for_2.1.png" alt="..." aria-label="4">
     <h1 class="h3" style="margin-top: 20px; margin-bottom: 5px;">Bootstrap 3 - Carousel (карусель)</h1>
     <h2 class="h4" style="margin-top: 0; margin-bottom: 20px;">без элементов управления</h2>
 
     <!-- Bootstrap 4 -->
     <div id="carousel" class="carousel slide carousel-fade" data-ride="carousel" aria-label="4">
-        <!-- Индикаторы -->
-        <ol class="carousel-indicators">
-            <li data-target="#carousel" data-slide-to="0" class="active"></li>
-            <li data-target="#carousel" data-slide-to="1"></li>
-            <li data-target="#carousel" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner zoomed">
-            <div class="carousel-item active">
-                <img class="img-fluid" src="../files/upload/images/for_2.1.png" alt="...">
-                <div class="carousel-caption">
-                    11111111
+        <table class="carousel-outer">
+            <!-- Индикаторы -->
+            <ol class="carousel-indicators">
+                <li data-target="#carousel" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel" data-slide-to="1"></li>
+                <li data-target="#carousel" data-slide-to="2"></li>
+            </ol>
+            <td class="carousel-inner zoomed">
+                <div class="carousel-item active">
+                    <img class="img-fluid" src="../files/upload/images/for_2.1.png" alt="...">
+                    <div class="carousel-caption">
+                        11111111
+                    </div>
                 </div>
-            </div>
-            <div class="carousel-item">
-                <img class="img-fluid" src="../files/upload/images/for_2.2.png" alt="...">
+                <div class="carousel-item">
+                    <img class="img-fluid" src="../files/upload/images/for_2.2.png" alt="...">
 
-            </div>
-            <div class="carousel-item">
-                <img class="img-fluid" src="../files/upload/images/for_2.3.png" alt="...">
-                <div class="carousel-caption">
-                    33333333
                 </div>
-            </div>
-        </div>
-        <!-- Элементы управления -->
-        <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Предыдущий</span>
-        </a>
-        <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Следующий</span>
-        </a>
+                <div class="carousel-item">
+                    <img class="img-fluid" src="../files/upload/images/for_2.3.png" alt="...">
+                    <div class="carousel-caption">
+                        33333333
+                    </div>
+                </div>
+            </td>
+            <!-- Элементы управления -->
+            <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Предыдущий</span>
+            </a>
+            <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Следующий</span>
+            </a>
+        </table>
     </div>
 </div>
 </body>
